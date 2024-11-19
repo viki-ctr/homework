bf023fd2b675302e12558a1732afd792755b90e0
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2024-11-19 23:02:35 +0300

    update function masks.py

--- src/masks.py
+++ src/masks.py
@@ -4,16 +4,16 @@ from typing import Union
 def get_mask_card_number(card_number: Union[str | int]) -> str:
     """Функция маскировки номера банковской карты"""
     card_number_str = str(card_number)
-    if len(card_number_str) == 16:
-        return f"{card_number_str[-16:-12]} {card_number_str[-12:-10]}** **** {card_number_str[-4:]} "
+    if card_number_str.isdigit() and len(card_number_str) == 16:
+        return f'{card_number_str[-16:-12]} {card_number_str[-12:-10]}** **** {card_number_str[-4:]}'
     else:
-        return "Please enter a valid value"
+        return 'Please enter a valid value'
 
 
 def get_mask_account(account_number: Union[str | int]) -> str:
     """Функция маскировки номера банковского счета"""
     account_number_str = str(account_number)
-    if len(account_number_str) == 20:
-        return f"**{account_number_str[-4:]}"
+    if account_number_str.isdigit() and len(account_number_str) == 20:
+        return f'**{account_number_str[-4:]}'
     else:
-        return "Please enter a valid value"
+        return 'Please enter a valid value'
