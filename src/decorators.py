73b56700cb388c3136bedfa11ee8eab1d9507107
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2024-12-02 00:54:19 +0300

    function decorators done

---
+++ src/decorators.py
@@ -0,0 +1,38 @@
+from functools import wraps
+from time import time
+from typing import  AnyStr
+# import os
+
+
+def log(filename=None):
+    """Декоратор, который автоматически логирует начало и конец выполнения функции, ее результаты/возникшие ошибки"""
+
+    def decorator(func):
+        @wraps(func)
+
+        def wrapper(*args: tuple[AnyStr], **kwargs: dict[str, AnyStr]):
+            try:
+                time_1 = time()
+                result = func(*args, **kwargs)
+                time_2 = time()
+                if filename == str and '.txt' in filename:
+                   with open(filename, "a") as file:
+                     file.write(f'{func.__name__} ok. \nStart time: {time_1}. \nEnd time: {time_2}')
+                else:
+                   print(f'{func.__name__} ok. \nStart time: {time_1}. \nEnd time: {time_2}')
+                return result
+            except Exception as e:
+                if filename:
+                    with open(filename, 'a', encoding='utf-8') as file:
+                        file.write(f'{func.__name__} ERROR : {e.__class__.__name__}. Inputs: {args}, {kwargs}\n')
+                else:
+                    print(f'{func.__name__} ERROR : {e.__class__.__name__}. Inputs: {args}, {kwargs}\n')
+        return wrapper
+    return decorator
+
+
+@log(filename="logs/mylog.txt")
+def my_function(x, y):
+    return x + y
+
+my_function(1, 2)
