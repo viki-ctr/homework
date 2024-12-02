2686c7e469796dbcffa6cd46791d8923a81e4d6f
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2024-12-03 00:16:54 +0300

    some update

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.decorators import my_function
+from src.decorators import log
 
 
 def test_successful_execution(capsys):
@@ -13,8 +14,10 @@ def test_error_handling(capsys):
     with pytest.raises(expected_exception=Exception):
         my_function(1, 'a')
     captured = capsys.readouterr()
-    assert "" in captured.out
+    assert '' in captured.out
 
 
-
+@log(filename="testdecor.txt")
+def test_error_function_with_file(*args, **kwargs):
+    raise ValueError("Something went wrong!")
 
