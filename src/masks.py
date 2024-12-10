46dda169ec425e4f19281cd4df3ec7f920f109f2
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2024-12-11 01:33:08 +0300

    code formatting check

--- src/masks.py
+++ src/masks.py
@@ -1,48 +1,55 @@
-from typing import Union
 import logging
 import os
-
+from typing import Union
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(base_dir, "..", "logs", "masks.log", )
+file_path = os.path.join(
+    base_dir,
+    "..",
+    "logs",
+    "masks.log",
+)
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    filename=file_path,encoding='utf-8',
-                    filemode='w')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename=file_path,
+    encoding="utf-8",
+    filemode="w",
+)
 
 
 def get_mask_card_number(card_number: Union[str | int]) -> str:
     """Функция маскировки номера банковской карты"""
     try:
-        logging.info('Преобразуем полученное значение карты в строку')
+        logging.info("Преобразуем полученное значение карты в строку")
         card_number_str = str(card_number)
 
         if card_number_str.isdigit() and len(card_number_str) == 16:
-            logging.info('Маскируем строку с номером карты')
-            return f'{card_number_str[-16:-12]} {card_number_str[-12:-10]}** **** {card_number_str[-4:]}'
+            logging.info("Маскируем строку с номером карты")
+            return f"{card_number_str[-16:-12]} {card_number_str[-12:-10]}** **** {card_number_str[-4:]}"
 
         else:
-            logging.info('Введены неверные данные')
-            return 'Please enter a valid value'
+            logging.info("Введены неверные данные")
+            return "Please enter a valid value"
 
     except Exception as ex:
-        logging.error(f'Произошла ошибка {ex}', exc_info=True)
+        logging.error(f"Произошла ошибка {ex}", exc_info=True)
 
 
 def get_mask_account(account_number: Union[str | int]) -> str:
     """Функция маскировки номера банковского счета"""
     try:
-        logging.info('Преобразуем полученное значение счета в строку')
+        logging.info("Преобразуем полученное значение счета в строку")
         account_number_str = str(account_number)
 
         if account_number_str.isdigit() and len(account_number_str) == 20:
-            logging.info('Маскируем строку с номером счета')
-            return f'**{account_number_str[-4:]}'
+            logging.info("Маскируем строку с номером счета")
+            return f"**{account_number_str[-4:]}"
 
         else:
-            logging.info('Введены неверные данные')
-            return 'Please enter a valid value'
+            logging.info("Введены неверные данные")
+            return "Please enter a valid value"
 
     except Exception as ex:
-        logging.error(f'Произошла ошибка {ex}', exc_info=True)
+        logging.error(f"Произошла ошибка {ex}", exc_info=True)
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,17 @@
 import json
-import os
 import logging
+import os
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path_log = os.path.join(base_dir, "..", "logs", "util.log")
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    filename=file_path_log,encoding='utf-8',
-                    filemode='w')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename=file_path_log,
+    encoding="utf-8",
+    filemode="w",
+)
 
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,21 +22,21 @@ def transaction(file=file_path_json):
     """Функция, принимающая путь до JSON-файла и возвращающая список словарей или пустой список"""
     with open(file, "r", encoding="UTF-8") as f:
         try:
-            logging.info('Преобразуем файл формата JSON в объект Python')
+            logging.info("Преобразуем файл формата JSON в объект Python")
             transactions_list = json.load(f)
 
             if len(transactions_list) == 0:
-                logging.info('Файл пустой')
+                logging.info("Файл пустой")
                 return []
 
             else:
-                logging.info('Файл в формате списка')
+                logging.info("Файл в формате списка")
                 return transactions_list
 
         except json.JSONDecodeError as ex:
-            logging.error(f'Произошла ошибка {ex}', exc_info=True)
+            logging.error(f"Произошла ошибка {ex}", exc_info=True)
             return []
 
 
-data = transaction(file=file_path_json)
-print(data)
+# data = transaction(file=file_path_json)
+# print(data)
